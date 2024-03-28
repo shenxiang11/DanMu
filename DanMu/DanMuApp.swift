be037835e72770ffa78c438f07bbae8a500ad7a3
Shen <863461783@qq.com>
2024-03-28 10:59:55 +0800

    Initial Commit

---
+++ DanMu/DanMuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanMuApp.swift
+//  DanMu
+//
+//  Created by 香饽饽zizizi on 2024/3/28.
+//
+
+import SwiftUI
+
+@main
+struct DanMuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
